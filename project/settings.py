d4bdf660bee9e83ff5aea5fff0c2fe73e5357021
Victor Amorim <victorsilva6075@gmail.com>
2022-12-20 22:17:55 -0300

    clear

--- project/settings.py
+++ project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r98#ntrrjfzpq)r63+k%bvg)us0t4odzj&ee-e9_e4#shb9_o_'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -42,10 +42,6 @@ INSTALLED_APPS = [
 ]
 
 AUTH_USER_MODEL = 'user.User'
-
-ROLES = {
-    'role_create_products': 'Permite cadastrar produtos no sistema'
-}
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
